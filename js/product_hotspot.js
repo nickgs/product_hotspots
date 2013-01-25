0a3c64806474d610c672fc68aaff9e19f65888fc
nickgs <nickgs1@gmail.com>
2013-01-25 10:17:34 -0500

    Add bullet and content fade in

--- js/product_hotspot.js
+++ js/product_hotspot.js
@@ -1,3 +1,14 @@
+//******************************************************************
+// File_Name:	product_hotspot.js
+//
+// Purpose:	This code is designed to show an array of hotspots 
+//				over a background image. Each hotspot includes
+//				coordinates and content that will be displayed 
+//				on mouse enter events.
+//
+// Created by: George Thomaidis
+//*******************************************************************
+
 $(function() { 
 	
 	//build data structure to store coordinates and content. 
@@ -6,27 +17,37 @@ $(function() {
 	product_hotspots[1] = {x: 500, y:500, content: "This is more test content"}
 	product_hotspots[2] = {x: 400, y:400, content: "This is even more test content"}
 	
+	var  hotspotFadeTimer = 1500; 		//Value reflects how fast the hotspots fade in;
+	var  contentFadeTimerIn = 500; 		//Value reflects how fast the content fades in;
+	var  contentFadeTimerOut = 500;		//Value reflects how fast the content fads out;
+
 	//loop through each hotspot. 
 	$.each(product_hotspots,function(idx) {
+
 		//append a new div to #product_image for each hotspot with the following format: 
 		var $newdiv1 = $('<div class="bullet" style="position: absolute; left:'+product_hotspots[idx].x+'px ; top:'+product_hotspots[idx].y+'px" data-content="'+product_hotspots[idx].content+'"><img src="images/hotspot.png"/></div>');
-		
-		//hint: 
-		$("#product_image").append($newdiv1);
+
+		//Adds hot spots to the coordinates set in the array
+		$("#product_image").append(($newdiv1).fadeIn(hotspotFadeTimer));
+		$(".bullet").css('cursor', 'pointer');
 		
 	});
 	
-	//create function to listen to rollover events and extract the data-content attribute and place in #rollover_content. 
-	$(".bullet").mouseover(function() {
-			
-			//store content of this bullet in the content variable. 
-			var content = $(this).attr("data-content");  
-			//alert(content);
-			//place content into the #rollover_content. 
-			//hint: 
-			$("#rollover_content").html(content);
-		
-	}); 
+	//This function listens to rollover events and extract the data-content attribute and place in #rollover_content. 
+	$(".bullet").mouseenter(function() {
+			//Grabs the data-content attached to the entered bullet from the array and stores it in the "$content" variable. 
+			var $content = $(this).attr("data-content");
+
+			//Adds html text content wrapped in a paragraph tag which is hidden until fade in shows it
+			$("#rollover_content").html("<p>" +$content).hide().fadeIn(contentFadeTimerIn);
+	});
+
+	//This function listens for mouse leave events and fades the content associated with it 
+	$(".bullet").mouseleave(function() {
+			//Fades out the hotpsot content
+			$("#rollover_content").fadeOut(contentFadeTimerOut);
+	});
+
 	
 	//The console is great for debugging:
 	console.log(product_hotspots); 
